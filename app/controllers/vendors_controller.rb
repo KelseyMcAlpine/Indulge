016aa318a5cdfb168755d2000fc6b8241985874f
Ann Dai <gofarann@gmail.com>
2017-04-19 15:55:02 -0700

    added show and index methods to vendor controller, added render 404 method to application controller

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,17 @@
 class VendorsController < ApplicationController
+
+  def index
+    @vendors = Vendor.all
+  end
+
+  def show
+    @vendor = Vendor.find_by_id params[:id]
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:username, :email)
+  end
+
 end
