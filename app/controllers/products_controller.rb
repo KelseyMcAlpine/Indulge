9c0fcc15c37354c9dfc3b84d248b2839671a5af8
Ann Dai <gofarann@gmail.com>
2017-04-18 18:32:48 -0700

    added show method to product controller, added private find product method too

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,22 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show]
+
 
   def index
     @products = Product.all
   end
 
-  
+  def show
+    if !@product
+      render_404
+    end
+  end
+
+
+end
+
+private
+
+def find_product
+  @product = Product.find_by_id params[:id]
 end
