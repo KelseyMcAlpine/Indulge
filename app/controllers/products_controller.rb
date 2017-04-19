4d922c43284f82da81c1518140cb4bcd1d2f510b
Ann Dai <gofarann@gmail.com>
2017-04-19 16:52:26 -0700

    added update and edit methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  before_action :find_product, only: [:show]
+  before_action :find_product, only: [:show, :edit, :update]
 
   def new
     @product = Product.new
@@ -27,7 +27,24 @@ class ProductsController < ApplicationController
       render_404
     end
   end
-  
+
+  def edit; end
+
+  def update
+    @product.price = product_params[:price]
+    @product.inventory = product_params[:inventory]
+    @product.name = product_params[:name]
+    @product.description = product_params[:description]
+    @product.photo_url = product_params [:photo_url]
+
+    if @product.save
+      flash[:success] = "Successfully updated #{@product.name}."
+      redirect_to product_path(@product.id) #this is the show page
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def product_params
