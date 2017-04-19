0cfc96397007989f34486d104e5f3b695ebc9947
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-18 19:55:50 -0700

    Created products#new and products#create controller actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create product_params
+
+    if @product.id != nil
+      flash[:success] = "New product successfull added"
+      redirect_to product_path(@product.id)
+    else
+      flash.now[:error] = "Hmm.. something went wrong."
+      render "new"
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:vendor_id, :price, :inventory, :name, :description, :photo_url, :lifecycle)
+  end
 end
