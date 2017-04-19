452d97b032cfe80cdc82f4e396e4307cbe837479
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-18 19:04:56 -0700

    Created vendor seeds and vendor validations

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,4 @@
 class Vendor < ApplicationRecord
+  validates :username, uniqueness: true, presence: true
+  validates :email, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+vendors = Vendor.create([
+  { username: "Dwight Schrute", email: "schrutefarms@gmail.com"},
+  { username: "Jim Halpert", email: "jim@athlead.com"},
+  { username: "Pam Beasley", email: "Pam@prattinstitute.com"},
+  { username: "Michael Scott", email: "mscott@greatscottproductions.com"},
+  { username: "Kelly Kapoor", email: "kkapoor@businessbitch.com"},
+  { username: "Ryan Howard", email: "rhoward@gwhuf.edu"},
+  { username: "Andy Bernard", email: "narddog@cornell.edu"},
+  { username: "Kevin Malone", email: "bigbog69@fanduel.com"},
+  { username: "Angela Martin", email: "msmartin@lipton4PA.com"},
+  { username: "Toby Flenderson", email: "chadflenderson@flendersonfiles.com"},
+  { username: "Creed Bratton", email: "notcreedbratton@creedthoughts.gov"},
+  { username: "David Wallace", email: "dwallace@suckit.com"},
+  ])
