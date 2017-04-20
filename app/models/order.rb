2d443e91d56fc7df55623e71d0dca7bb4191130f
Regan Huff <regan.huff@gmail.com>
2017-04-20 15:25:35 -0700

    Fix order product relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
-  has_and_belongs_to_many :products
+  has_many :order_products
+  has_many :products, through: :order_products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ApplicationRecord
   has_many :reviews
   belongs_to :vendor
   has_and_belongs_to_many :categories
-  has_and_belongs_to_many :orders
+  has_many :order_products
+  has_many :orders, through: :order_products
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
