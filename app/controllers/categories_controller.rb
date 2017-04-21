6618d313377ae29552a55f98e7f06d2524fc7f92
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-21 12:40:12 -0700

    Created tests for category model and controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,27 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create category_params
+
+    if @category.id != nil
+      flash[:success] = "New category successfull added"
+      redirect_to root_path
+      # this should redirect to category index page once created
+    else
+      flash.now[:error] = "Hmm.. something went wrong."
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
