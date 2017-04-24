013affddcb9c955473767fb51ad017b9419d212f
Haby Randall <sow.haby@gmail.com>
2017-04-24 14:58:13 -0700

    Working on create session method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
 
   def create
-   auth_hash = request.env['omniauth.auth']
-   raise
- end
-end
+    auth_hash = request.env['omniauth.auth']
+
+    vendor = Vendor.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+
+
+    if vendor.nil?
+      vendor = Vendor.create_from_github(auth_hash)
+
+      if vendor.nil?
+        flash[:error] = "Could not log you in"
+        redirect_back(fallback_location: root_path)
+      end
+    end
+  end
+end 
