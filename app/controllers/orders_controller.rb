d67f639c595fc643debe10814b360d343b403264
Ann Dai <gofarann@gmail.com>
2017-04-24 15:59:14 -0700

    added stuff to order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,33 @@ class OrdersController < ApplicationController
     vendor = Vendor.find_by(id: params[:vendor_id])
     @orders = vendor.orders
   end
+
+  def new
+    @order = Order.new
+  end
+
+  def add
+    #check availability first
+    order = Order.find_by_id [params:[:id]
+  end
+
+  def remove
+  end
+
+  def create
+    @order = Order.create order_params
+    unless @order.id == nil
+      flash[:sucess] = "Your order has been succesfully submitted"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Error occured, try again".
+      render "new"
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:purchase_date, :status, :cust_email, :cust_address, :credit_card, :cc_expire)
+  end
 end
