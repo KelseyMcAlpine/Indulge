94cd7e0b10659fadf2c1723171ae97d9be7fc588
Ann Dai <gofarann@gmail.com>
2017-04-24 11:17:08 -0700

    fixed create method on product

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,16 @@ Rails.application.routes.draw do
   get 'reviews/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
   resources :products
+  root 'products#index'
   post "/products/:id", to: "products#update_availability", as: "update_availability"
 
+
   resources :vendors, only: [:index, :show]
+
+  # do
+  #   get 'vendors/account/:id/products'
+  # end
   get "vendors/account/:id", to: "vendors#account", as: "vendor_account"
 
   resources :categories, only: [:index, :new, :create] do
