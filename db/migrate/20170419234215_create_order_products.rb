e409312407510c20deca0c068884e89ce5035f90
Ann Dai <gofarann@gmail.com>
2017-04-24 14:15:12 -0700

    deleted orders products from old migration

--- db/migrate/20170419234215_create_order_products.rb
+++ db/migrate/20170419234215_create_order_products.rb
@@ -1,6 +1,5 @@
 class CreateOrderProducts < ActiveRecord::Migration[5.0]
   def change
-    drop_table :orders_products
     create_table :orders_products do |t|
       t.belongs_to :product, index: true
       t.belongs_to :order, index: true
