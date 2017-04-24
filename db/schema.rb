16c5f9f651ae7b4ed19f41e0a1439a0a80ae59be
Regan Huff <regan.huff@gmail.com>
2017-04-24 06:02:52 -0700

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420164847) do
+ActiveRecord::Schema.define(version: 20170424125313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,15 +62,6 @@ ActiveRecord::Schema.define(version: 20170420164847) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["vendor_id"], name: "index_products_on_vendor_id", using: :btree
-  end
-
-  create_table "products_categories", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_products_categories_on_category_id", using: :btree
-    t.index ["product_id"], name: "index_products_categories_on_product_id", using: :btree
   end
 
   create_table "reviews", force: :cascade do |t|
