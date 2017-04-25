79e55202c83edc9301efdfc64d604dd1090df2f5
Regan Huff <regan.huff@gmail.com>
2017-04-25 13:01:24 -0700

    Delete old products by category action and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,17 +28,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def product_list
-    @category = Category.find_by_id(params[:category_id])
-
-    if params[:category_id]
-      @products = Product.includes(:categories).where(categories: { id: params[:category_id]} )
-      # if @products == []
-      #   flash[:error] = "There are no products in that category."
-      # end
-    end
-  end
-
 
   private
 
