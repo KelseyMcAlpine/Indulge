0a05eaf36cde962b9005a69ea41034df69cda5c2
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-25 09:21:02 -0700

    spaces!

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 20170424214625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+
+
+
+
+
+
+
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
