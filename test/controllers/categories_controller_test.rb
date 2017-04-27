9550b9e72bdbf4dd5d24364e34bdbe46eeabed4d
Regan Huff <regan.huff@gmail.com>
2017-04-27 06:47:12 -0700

    Revise tests for new category action to include login

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,26 +3,35 @@ require "test_helper"
 describe CategoriesController do
   let(:spa) {categories(:spa)}
 
+  describe "User is not logged in" do
+
   it "should get index" do
     get categories_path
     must_respond_with :success
   end
 
-  it "should get form to create a new category" do
-    get new_category_path
-    must_respond_with :success
   end
 
-  it "should affect the model when a new category is added" do
-    proc { post categories_path, params:
-      { category: { name: "spicy" } }}.must_change 'Category.count', 1
-  end
+  describe "User/Vendor is logged in" do
+    before do
+      login_user(vendors(:Jim))
+    end
 
-  it "should redirect to category index page after adding a new category" do skip
-    post products_path, params: { categories:
-      { name: "yummy"}
-    }
-    must_redirect_to category_show_path
-    # will need to double check that this is the correct paht once it's been created
+
+    it "should get form to create a new category" do
+      get new_category_path
+      must_respond_with :success
+    end
+
+    it "should affect the model when a new category is added" do
+      proc { post categories_path, params:
+        { category: { name: "spicy" } }}.must_change 'Category.count', 1
+    end
+
+    it "should redirect to category index page after adding a new category" do
+      post categories_path, params: { category: { name: "yummy"}}
+      must_redirect_to categories_path
+    end
+
   end
 end
