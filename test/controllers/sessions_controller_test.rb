f453fa8fd60b76badd5ea444b44496beeb76d6e1
Regan Huff <regan.huff@gmail.com>
2017-04-27 05:50:48 -0700

    Initial sessions controller test and helper methods

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 describe SessionsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "auth_callback" do
+    it "logs in an existing vendor and redirects to vendor account" do
+      proc {
+        login_user(users(:Michael))
+        must_redirect_to vendor_account_path
+        session[:vendor_id].must_equal users(:Michael).id
+      }.must_change 'User.count', 0
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
