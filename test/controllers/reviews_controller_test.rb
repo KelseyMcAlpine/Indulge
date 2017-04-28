08c841cc3a7c07f5f3264653ba9c431fb0ad6578
Haby Randall <sow.haby@gmail.com>
2017-04-28 00:01:45 -0700

    Writing review controller tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
 describe ReviewsController do
-  it "should get index" do skip
-    get reviews_index_url
-    value(response).must_be :success?
+
+  it "should get new" do
+  product = products(:ice_floe)
+  review = reviews(:first_review)
+  proc {
+    get new_product_review_path, params:  { review:
+      {id: product.id,
+      }
+    }
+  }
+    must_respond_with :success
   end
 
   it "should get new" do skip
