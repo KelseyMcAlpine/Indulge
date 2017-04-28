269a542ba5ea7cb1a8d1e5a50d520cabf406f768
Regan Huff <regan.huff@gmail.com>
2017-04-28 09:45:51 -0700

    Trying again

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -7,9 +7,9 @@ class Vendor < ApplicationRecord
   def self.create_from_github(auth_hash)
     vendor = Vendor.new
 
-    if auth_hash["uid"] == nil || auth_hash["provider"] == nil || auth_hash["info"] == nil
-      return nil
-    end
+    # if auth_hash["uid"] == nil || auth_hash["provider"] == nil || auth_hash["info"] == nil
+    #   return nil
+    # end
 
     vendor.uid = auth_hash["uid"]
     vendor.provider = auth_hash["provider"]
@@ -17,9 +17,9 @@ class Vendor < ApplicationRecord
     vendor.email = auth_hash["info"]["email"]
 
     # if name not supplied by github, create unique username
-    if vendor.username == nil || vendor.username == ""
-      vendor.username = self.generate_username
-    end
+    # if vendor.username == nil || vendor.username == ""
+    #   vendor.username = self.generate_username
+    # end
 
     vendor.save ? vendor : nil
   end
