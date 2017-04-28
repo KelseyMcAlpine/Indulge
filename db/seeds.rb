4064a0376b16632168d89f4cd49e995c29c77279
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-27 20:27:23 -0700

    Updated seed product images. Added top products to index

--- db/seeds.rb
+++ db/seeds.rb
@@ -88,7 +88,7 @@ vendors = Vendor.create ([
       price: 1800,
       inventory: 4,
       description: "This voluptuous feast of sea lion flipper with braised lichen will be served with extraordinary company in a sublime setting.",
-      photo_url: "http://i.telegraph.co.uk/multimedia/archive/03339/polar-bear-global-_3339474b.jpg",
+      photo_url: "https://unsplash.it/801/800",
       lifecycle: "available"
     },
     {
@@ -97,7 +97,7 @@ vendors = Vendor.create ([
       price: 550,
       inventory: 2,
       description: "Enjoy the sonic assault of high flight in close quarters with some of humankind's most emphatic voices; rose in a can and light cocktail snacks provided.",
-      photo_url: "http://www.printsoldandrare.com/airplanes/043air.jpg",
+      photo_url: "https://unsplash.it/800/801",
       lifecycle: "available"
     },
     {
@@ -106,7 +106,7 @@ vendors = Vendor.create ([
       price: 150,
       inventory: 15,
       description: "Puget Sound transformed through the eyes of a short but passionate local.",
-      photo_url: "http://1.bp.blogspot.com/-Yv1hfz7vzeU/VTfT5RCDtII/AAAAAAAAAKw/MtKWJClIntI/s1600/Samish_helo.jpg",
+      photo_url: "https://unsplash.it/798/800",
       lifecycle: "available"
     },
     {
@@ -115,7 +115,7 @@ vendors = Vendor.create ([
       price: 250,
       inventory: 6,
       description: "An extravagant yet ethical feast featuring only ingredients sourced from our 25' x 25' back yard.",
-      photo_url: "http://bpc.h-cdn.co/assets/16/20/980x490/landscape-1463681075-green-chef-vegan-meals.jpg",
+      photo_url: "https://unsplash.it/800/798",
       lifecycle: "available"
     },
     {
@@ -124,7 +124,7 @@ vendors = Vendor.create ([
       price: 450,
       inventory: 25,
       description: "A full-day excursion to my dad's poorly ventilated garage workshop, where you'll hold wrenches, fetch solvent, and listen to baseball on the radio while deepening your knowledge of the 1965 V8 Convertible.",
-      photo_url: "http://www.onlymustangfords.com/images/1965-mustang-restoration-front.jpg",
+      photo_url: "https://unsplash.it/799/800",
       lifecycle: "available"
     },
     {
@@ -133,7 +133,7 @@ vendors = Vendor.create ([
       price: 450,
       inventory: 25,
       description: "A full-day excursion to my dad's poorly ventilated garage workshop, where you'll hold wrenches, fetch solvent, and listen to baseball on the radio while deepening your knowledge of the 1965 V8 Convertible.",
-      photo_url: "http://www.onlymustangfords.com/images/1965-mustang-restoration-front.jpg",
+      photo_url: "https://unsplash.it/800/799",
       lifecycle: "available"
     }
     ])
