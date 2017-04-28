32dd87fad387f72a979b5891ad313f2711b0ac01
Ann Dai <gofarann@gmail.com>
2017-04-27 17:11:32 -0700

    added vendor controller tests

--- test/controllers/vendors_controller_test.rb
+++ test/controllers/vendors_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 describe VendorsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "user not logged in" do
+
+    it "should get index" do
+
+    end
+
+    it "should get new" do
+
+    end
+
+    it "should get create" do
+
+    end
+
+  end
+
+  describe "user logged id" do
+
+    it "should get order management page" do
+    end
+
+    it "should get product management page" do
+
+    end
+
+
+  end
+
 end
