2a5ad4998dc8e5c9ecc7f99cf50fe5cc2d2cd4b9
Haby Randall <sow.haby@gmail.com>
2017-04-28 00:04:31 -0700

    Writing create reviews test

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -4,7 +4,7 @@ describe ReviewsController do
 
   it "should get new" do
   product = products(:ice_floe)
-  review = reviews(:first_review)
+
   proc {
     get new_product_review_path, params:  { review:
       {id: product.id,
@@ -14,14 +14,14 @@ describe ReviewsController do
     must_respond_with :success
   end
 
-  it "should get new" do skip
-    get reviews_new_url
-    value(response).must_be :success?
-  end
-
-  it "should get create" do skip
-    get reviews_create_url
-    value(response).must_be :success?
+  it "should get create" do 
+    proc {
+      get product_reviews_path, params:  { review:
+        {id: product.id,
+        }
+      }
+    }
+      must_respond_with :success
   end
 
 
